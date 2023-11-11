f3bf9c4d90264aa831dfab1fdb4fdfccdc2040cf
lbrezgin <lewickbrez@gmail.com>
2023-11-11 18:28:02 +0200

    feat: 4 task done

--- school/coding/prog4/video.cpp
+++ school/coding/prog4/video.cpp
@@ -1,1 +1,12 @@
+
 #include <iostream>
+using namespace std;
+int main(){
+   cout<<"\n\n\n";
+   cout<<"\t |\\_____/|\n";
+   cout<<"\t |       |\n";
+   cout<<"\t |[o] [o]|\n";
+   cout<<"\t |   v   |\n";
+   cout<<"\t |       |\n";
+   cout<<"\t-ooo---ooo-";
+}
